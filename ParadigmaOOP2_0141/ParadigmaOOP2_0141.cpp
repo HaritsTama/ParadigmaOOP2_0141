536787e43cc256c0016838c261a5b33f2bd2544f
HaritsTama <nayotama1818@gmail.com>
2024-05-27 10:02:25 +0700

    mmebuat class turunan lia

--- ParadigmaOOP2_0141/ParadigmaOOP2_0141.cpp
+++ ParadigmaOOP2_0141/ParadigmaOOP2_0141.cpp
@@ -14,5 +14,18 @@ public:
 class joko : public seseorang
 {
 public :
-
+	void pesan()
+	{
+		cout << "Pesan dari joko" << endl;
+	}
 };
+
+class lia : public seseorang
+{
+public:
+	void pesan()
+	{
+		cout << "Pesan dari lia" << endl;
+	}
+};
+

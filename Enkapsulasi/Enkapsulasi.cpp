6530f5dfc73fb240db349f0dca1dd68a1e78368b
HaritsTama <nayotama1818@gmail.com>
2024-05-27 10:16:10 +0700

    membuat fungsi main

--- Enkapsulasi/Enkapsulasi.cpp
+++ Enkapsulasi/Enkapsulasi.cpp
@@ -15,3 +15,15 @@ public :
 		return saklarNo[i];
 	}
 };
+
+int main()
+{
+	remoteLampu lampuRumah;
+
+	lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+	lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+	lampuRumah.setSaklarNo(2, "Lampu Kmaar Tidur");
+	lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+	return 0;
+}
